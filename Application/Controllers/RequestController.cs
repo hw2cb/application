f85727b7e3fbf0c56f22fbf48abe58e7537082cb
hw2cb <dim5.dim5.960@gmail.com>
2021-05-25 00:27:17 +0400

    25

--- Application/Controllers/RequestController.cs
+++ Application/Controllers/RequestController.cs
@@ -9,9 +9,11 @@ namespace Application.Controllers
     public class RequestController : Controller
     {
         RequestRepository repo;
+        SubjectRepository subjectsRepo;
         public RequestController()
         {
             repo = new RequestRepository();
+            subjectsRepo = new SubjectRepository();
         }
         //
         // GET: /Request/
@@ -24,6 +26,45 @@ namespace Application.Controllers
         {
             Request request = repo.GetRequests().FirstOrDefault(x => x.Id == requestId);
             return View(request);
+        }
+
+        [HttpPost]
+        public ActionResult CreateReport(int month)
+        {
+            ViewBag.SelItemsMon = GetSelectItems();
+
+            int year = DateTime.Now.Year;
+            List<Item> repItem = new List<Item>();//объединяем из всех нужных отчетов
+            
+            List<Request> sought = new List<Request>(); //искомые отчеты
+            sought.AddRange(repo.GetRequests().Where(x => x.Date.Month == month).Where(x => x.Date.Year == year));
+            foreach(var i in sought) 
+            {
+                repItem.AddRange(i.Items);
+            }
+            List<Item> items = new List<Item>();
+            for(int i=0; i<repItem.Count; i++)
+            {
+            }
+            Report report = new Report() { Subjects = items, Date = new DateTime(year, month, DateTime.Now.Day)};
+            //report.Date = new DateTime(year, month, DateTime.Now.Day);
+          
+            return View("Report", report);
+        }
+        public ActionResult Report()
+        {
+            ViewBag.SelItemsMon = GetSelectItems();
+            return View();
+        }
+        public SelectList GetSelectItems()
+        {
+            List<SelectListItem> SelectItems = new List<SelectListItem>();
+            for (int i = 0; i < 12;i++ )
+            {
+                var item = new SelectListItem() { Selected = true, Text = Convert.ToString(i), Value = Convert.ToString(i) };
+                SelectItems.Add(item);
+            }
+            return new SelectList(SelectItems, "Value", "Text");
         }
 	}
 }
---
+++ Application/Models/Report.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Application.Models
+{
+    public class Report
+    {
+        public List<Item> Subjects { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
